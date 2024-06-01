6e6c5eaae751ab601cca155c099a238ae0fea7cd
Mateusz Samek <samekmat@gmail.com>
2024-06-01 18:40:13 +0200

    fix[measurements factories]: fixed factory fields

--- measurements/factories.py
+++ measurements/factories.py
@@ -10,14 +10,10 @@ class MeasurementFactory(factory.django.DjangoModelFactory):
 
     system = factory.SubFactory(HydroponicsSystemFactory)
 
-    pH_data = factory.Faker(
-        "pydecimal", left_digits=1, right_digits=2, positive=True, min_value=0, max_value=14
-    )
+    pH_data = factory.Faker("pydecimal", left_digits=2, right_digits=2, min_value=0, max_value=14)
 
     water_temperature = factory.Faker(
-        "pydecimal", left_digits=2, right_digits=2, positive=True, min_value=0, max_value=45
+        "pydecimal", left_digits=2, right_digits=2, min_value=0, max_value=45
     )
 
-    TDS = factory.Faker(
-        "pydecimal", left_digits=3, right_digits=2, positive=True, min_value=0, max_value=2000
-    )
+    TDS = factory.Faker("pydecimal", left_digits=4, right_digits=2, min_value=0, max_value=2000)
