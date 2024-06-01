d6feaea43e92c0c5d2e86caca5947cff3e51926c
Mateusz Samek <samekmat@gmail.com>
2024-06-01 18:54:38 +0200

    test[measurements]: add url tests

---
+++ measurements/tests/test_urls.py
@@ -0,0 +1,10 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+from measurements.views import MeasurementListCreate
+
+
+class MeasurementUrlTest(SimpleTestCase):
+    def test_list_url_resolves(self):
+        url = reverse("measurement-list-create")
+        self.assertEqual(resolve(url).func.cls, MeasurementListCreate)
