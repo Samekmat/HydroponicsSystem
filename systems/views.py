62dd264b56316dde8bbb43dbbccb4718b789201a
Mateusz Samek <samekmat@gmail.com>
2024-06-01 14:37:49 +0200

    feat[systems]: rewrite views into ModelViewSet to keep CRUD in one view, also add docstrings for them and use router to handle the viewset

--- systems/views.py
+++ systems/views.py
@@ -1,21 +1,36 @@
-from rest_framework import generics, permissions
+from rest_framework import permissions, viewsets
 from systems.models import HydroponicsSystem
 from systems.serializers import HydroponicsSystemSerializer
 
 
-class HydroponicsSystemListCreate(generics.ListCreateAPIView):
-    queryset = HydroponicsSystem.objects.all()
-    serializer_class = HydroponicsSystemSerializer
-    permission_classes = [permissions.IsAuthenticated]
+class HydroponicsSystemViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing HydroponicsSystem instances.
 
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+    This viewset provides `list`, `create`, `retrieve`, `update`, and `destroy` actions.
+    Users must be authenticated to interact with the HydroponicsSystem instances.
+    """
 
-
-class HydroponicsSystemDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = HydroponicsSystem.objects.all()
     serializer_class = HydroponicsSystemSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return self.queryset.filter(owner=self.request.user)
+        """
+        Restricts the returned HydroponicsSystem instances to those owned by the current user.
+
+        Returns:
+            QuerySet: A queryset of HydroponicsSystem instances filtered by
+             the current authenticated user.
+        """
+        return HydroponicsSystem.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        """
+        Sets the owner of the HydroponicsSystem instance
+         to the current authenticated user upon creation.
+
+        Args:
+            serializer (HydroponicsSystemSerializer): The serializer instance to save.
+        """
+        serializer.save(owner=self.request.user)
