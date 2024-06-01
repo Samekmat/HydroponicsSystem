4c13741e05c39e24d6ada6a882b381f49a7a28d6
Mateusz Samek <samekmat@gmail.com>
2024-06-01 15:54:42 +0200

    feat[measurements]: add ListCreateAPIView with docstrings and adjust urls

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("systems.urls")),
+    path("api/measurements/", include("measurements.urls")),
 ]
