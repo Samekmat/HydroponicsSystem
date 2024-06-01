d6a7ebed231e86d67a57671ca587dba6d4fab1e8
Mateusz Samek <samekmat@gmail.com>
2024-06-01 14:16:17 +0200

    feat: add datetime fields to a systems model and owner field help text

--- systems/models.py
+++ systems/models.py
@@ -3,9 +3,13 @@ from django.db import models
 
 
 class HydroponicsSystem(models.Model):
-    owner = models.ForeignKey(User, related_name="systems", on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        User, related_name="systems", on_delete=models.CASCADE, help_text="Owner of the system."
+    )
     name = models.CharField(max_length=64)
     description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
