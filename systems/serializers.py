c5088d36e28070ae06663a36466c3ba8abac6b64
Mateusz Samek <samekmat@gmail.com>
2024-06-02 18:42:54 +0200

    feat: create serializer to handle systems detail with 10 last connected measurements

--- systems/serializers.py
+++ systems/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+
+from measurements.serializers import MeasurementSerializer
 from systems.models import HydroponicsSystem
 
 
@@ -6,4 +8,13 @@ class HydroponicsSystemSerializer(serializers.ModelSerializer):
     class Meta:
         model = HydroponicsSystem
         fields = ["owner", "name", "description"]
+        read_only_fields = ["owner", "created_at", "updated_at"]
+
+
+class HydroponicsSystemDetailSerializer(serializers.ModelSerializer):
+    measurements = MeasurementSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = HydroponicsSystem
+        fields = ["owner", "name", "description", "measurements"]
         read_only_fields = ["owner", "created_at", "updated_at"]
