829ed9f5575a789cbfe6290051ee4c524c82d125
Mateusz Samek <samekmat@gmail.com>
2024-06-02 19:30:40 +0200

    test(systems): adjust test for using simpleJWT

--- systems/tests/test_views.py
+++ systems/tests/test_views.py
@@ -1,6 +1,8 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+from rest_framework_simplejwt.tokens import AccessToken
+
 from auth_app.factories import UserFactory
 from systems.factories import HydroponicsSystemFactory
 from systems.models import HydroponicsSystem
@@ -9,18 +11,19 @@ from systems.models import HydroponicsSystem
 class HydroponicsSystemViewSetTest(APITestCase):
     def setUp(self):
         self.user = UserFactory()
-        self.client.force_login(self.user)
+        self.token = AccessToken.for_user(self.user)
         self.system = HydroponicsSystemFactory(owner=self.user)
 
     def test_list_hydroponics_systems(self):
         url = reverse("system-list")
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + str(self.token))
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]["name"], self.system.name)
+        self.assertTrue(response.data)
 
     def test_create_hydroponics_system(self):
         url = reverse("system-list")
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + str(self.token))
         data = {"name": "New System", "description": "A new hydroponics system"}
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -29,12 +32,14 @@ class HydroponicsSystemViewSetTest(APITestCase):
 
     def test_retrieve_hydroponics_system(self):
         url = reverse("system-detail", args=[self.system.id])
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + str(self.token))
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["name"], self.system.name)
 
     def test_update_hydroponics_system(self):
         url = reverse("system-detail", args=[self.system.id])
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + str(self.token))
         data = {"name": "Updated System", "description": self.system.description}
         response = self.client.put(url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -43,28 +48,26 @@ class HydroponicsSystemViewSetTest(APITestCase):
 
     def test_delete_hydroponics_system(self):
         url = reverse("system-detail", args=[self.system.id])
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + str(self.token))
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(HydroponicsSystem.objects.count(), 0)
 
     def test_list_hydroponics_systems_unauthenticated(self):
-        self.client.logout()
         url = reverse("system-list")
         response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_create_hydroponics_system_unauthenticated(self):
-        self.client.logout()
         url = reverse("system-list")
         data = {"name": "New System", "description": "A new hydroponics system"}
         response = self.client.post(url, data)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_retrieve_hydroponics_system_unauthenticated(self):
-        self.client.logout()
         url = reverse("system-detail", args=[self.system.id])
         response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_update_hydroponics_system_unauthorized(self):
         other_user = UserFactory()
@@ -72,17 +75,18 @@ class HydroponicsSystemViewSetTest(APITestCase):
         url = reverse("system-detail", args=[other_system.id])
         data = {"name": "Updated System", "description": other_system.description}
         response = self.client.put(url, data)
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_delete_hydroponics_system_unauthorized(self):
         other_user = UserFactory()
         other_system = HydroponicsSystemFactory(owner=other_user)
         url = reverse("system-detail", args=[other_system.id])
         response = self.client.delete(url)
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_create_hydroponics_system_empty_name(self):
         url = reverse("system-list")
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + str(self.token))
         data = {"name": "", "description": "A hydroponics system with an empty name"}
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
@@ -90,6 +94,7 @@ class HydroponicsSystemViewSetTest(APITestCase):
 
     def test_create_hydroponics_system_empty_description(self):
         url = reverse("system-list")
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + str(self.token))
         data = {"name": "Empty Description System", "description": ""}
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
@@ -97,6 +102,7 @@ class HydroponicsSystemViewSetTest(APITestCase):
 
     def test_create_hydroponics_system_maximum_length_name(self):
         url = reverse("system-list")
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + str(self.token))
         data = {"name": "A" * 64, "description": "A system with a maximum length name"}
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -104,6 +110,7 @@ class HydroponicsSystemViewSetTest(APITestCase):
 
     def test_create_hydroponics_system_exceeded_name_length(self):
         url = reverse("system-list")
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + str(self.token))
         data = {"name": "A" * 65, "description": "A description exceeding the maximum length"}
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
