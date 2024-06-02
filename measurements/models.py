c2b7adae36ef80b72df1979266d650dda11217f6
Mateusz Samek <samekmat@gmail.com>
2024-06-02 20:05:12 +0200

    refactor(RegisterView): add typing

--- measurements/models.py
+++ measurements/models.py
@@ -5,40 +5,40 @@ from systems.models import HydroponicsSystem
 
 
 class Measurement(models.Model):
-    system = models.ForeignKey(
+    system: models.ForeignKey[HydroponicsSystem] = models.ForeignKey(
         HydroponicsSystem,
         related_name="measurements",
         on_delete=models.CASCADE,
         help_text="The hydroponics system associated with this measurement.",
     )
 
-    pH_data = models.DecimalField(
+    pH_data: models.DecimalField = models.DecimalField(
         max_digits=4,
         decimal_places=2,
         validators=[MinValueValidator(0), MaxValueValidator(14)],
         help_text="The pH level of the nutrient solution, should be between 0 and 14.",
     )
 
-    water_temperature = models.DecimalField(
+    water_temperature: models.DecimalField = models.DecimalField(
         max_digits=5,
         decimal_places=2,
         validators=[MinValueValidator(0), MaxValueValidator(45)],
         help_text="The water temperature in Celsius, should be between 0°C and 45°C.",
     )
 
-    TDS = models.DecimalField(
+    TDS: models.DecimalField = models.DecimalField(
         max_digits=6,
         decimal_places=2,
         validators=[MinValueValidator(0), MaxValueValidator(2000)],
         help_text="The Total Dissolved Solids (TDS) in ppm, should be between 0 and 2000 ppm.",
     )
 
-    measured_at = models.DateTimeField(
+    measured_at: models.DateTimeField = models.DateTimeField(
         auto_now_add=True, help_text="The date and time this measurement was taken."
     )
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Measurement for {self.system} at {self.measured_at}"
 
     class Meta:
-        ordering = ["-measured_at"]
+        ordering: list = ["-measured_at"]
